f78c995bde0a9ae3d57df425e198ffd1d05fea17
Martin Schnabel <mcnilz@gmail.com>
2016-06-02 13:06:23 +0200

    bugfix

--- apps/widget/modules/widget/actions/actions.class.php
+++ apps/widget/modules/widget/actions/actions.class.php
@@ -153,7 +153,7 @@ class widgetActions extends policatActions
     }
 
     $subscribe_text = trim($this->petition_text['subscribe_text']);
-    if ($subscribe_text && mb_strpos($subscribe_text, '#')) {
+    if ($subscribe_text && mb_strpos($subscribe_text, '#') !== false) {
       $subscribe_text = strtr($subscribe_text, $this->widget->getDataOwnerSubst(' ', $this->petition));
     }
 
