c070f4a559e7aef7d6caf1dbbfb3e8f541aee6bd
Damian Ostraszewski <ostraszewski@o2.pl>
2016-06-03 13:01:04 +0200

    Petition / Media Files => update list display filename

--- apps/frontend/modules/d_media_files/templates/_list.php
+++ apps/frontend/modules/d_media_files/templates/_list.php
@@ -18,7 +18,7 @@
                 <tr>                    
                     <td><a href="<?php echo $file->getUrl(); ?>" title="<?php echo $file->getName() ?>" target="_blank"><img src="<?php echo $file->getUrl(); ?>"  width="100px" height="100px"/></a></td>
                     <td><?php echo format_date($file->getCreatedAt(), 'yyyy-MM-dd') ?></td>
-                    <td><?php echo $file->getName() ?></td>                                                        
+                    <td><?php echo $file->getFilename() ?></td>                                                        
                     <td><?php echo formatSize($file->getSize()) ?></td>                                    
                     <td>                                          
                             <a class="btn btn-mini" href="<?php echo url_for('media_files_rename', array('petition_id' => $petition, "id"=>$file->getId()))  ?>">Rename</a>
