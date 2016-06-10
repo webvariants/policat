c5ec49c8e7c74056e30e0af2bd96a5ed40f6b518
Martin Schnabel <mcnilz@gmail.com>
2016-06-10 17:35:19 +0200

    reponsive fix

--- apps/widget/templates/clean.php
+++ apps/widget/templates/clean.php
@@ -3,6 +3,7 @@
 ?><!DOCTYPE html>
 <html>
   <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <?php
     $portal_name = StoreTable::value(StoreTable::PORTAL_NAME);
     $title = $sf_response->getTitle();
