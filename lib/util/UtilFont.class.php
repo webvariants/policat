f3d45cabc841722de79b9af9cc6c680be186b7ae
Damian Dzieduch <d.dzieduch@gmail.com>
2016-09-09 10:31:44 +0200

    repair font name typo

--- lib/util/UtilFont.class.php
+++ lib/util/UtilFont.class.php
@@ -17,7 +17,7 @@ class UtilFont {
       'Arial, Helvetica, sans-serif', '"Arial Black", Gadget, sans-serif', '"Comic Sans MS", cursive, sans-serif',
       'Impact, Charcoal, sans-serif', '"Lucida Sans Unicode", "Lucida Grande", sans-serif', 'Tahoma, Geneva, sans-serif',
       '"Trebuchet MS", Helvetica, sans-serif', 'Verdana, Geneva, sans-serif', '"Courier New", Courier, monospace',
-      '"Lucida Console", Monaco, monospace', '"Lucida Sans Unicode", Vardana, Arial',
+      '"Lucida Console", Monaco, monospace', '"Lucida Sans Unicode", Verdana, Arial',
       '"Open Sans", sans-serif',
       'Roboto, sans-serif'
   );
