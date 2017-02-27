4a4dadcfb81b11c8142f9a5bcc7da5ea1c5ba6f7
Martin Schnabel <mcnilz@gmail.com>
2017-02-27 08:56:24 +0100

    show tips in menu of dasboard

--- apps/frontend/modules/d_home/templates/_menu.php
+++ apps/frontend/modules/d_home/templates/_menu.php
@@ -30,6 +30,7 @@
           <li><a href="<?php echo url_for('action_index') ?>">Actions</a></li>
           <li><a href="<?php echo url_for('widget_index') ?>">Widgets</a></li>
           <?php if ($pricing): ?><li><a href="<?php echo url_for('pricing') ?>">Pricing</a></li><?php endif ?>
+          <?php if ($tips): ?><li><a href="<?php echo url_for('tips') ?>"><?php echo $tips_title ?></a></li><?php endif ?>
           <li><a href="<?php echo url_for('help') ?>"><?php echo $help_title ?></a></li>
         <?php endif ?>
       </ul>
