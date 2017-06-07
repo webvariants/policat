ec3529f5644ebb091b4fbd6b33e8f2f707bff1c9
Damian Dzieduch <d.dzieduch@gmail.com>
2017-06-07 15:26:17 +0200

    solves the problem of selecting more than one option

--- lib/model/doctrine/ContactTable.class.php
+++ lib/model/doctrine/ContactTable.class.php
@@ -105,9 +105,20 @@ class ContactTable extends Doctrine_Table {
         }
       } else {
         if ($sel === 'contact') {
-          if (is_numeric($ts_1)) {
-            $query->andWhere("c.id = ?", $ts_1);
+          
+          $selected_options = explode(",",$ts_1);
+          
+          $first = true;
+          foreach (((array) $selected_options) as $option) {
+              
+              if($first) {
+                $query->andWhere("c.id = ?", $option);
+                $first = false;
+              } else {
+                $query->orWhere("c.id = ?", $option);
+              }
           }
+          
         } else {
           if (is_string($ts_1) && $ts_1 != 'all') {
             $query->andWhere("c.$sel = ?", $ts_1);
