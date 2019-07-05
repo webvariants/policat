585fe0cf2cb8fb0260a19acbf55a2da6e3267630
Martin Schnabel <martin@webvariants.de>
2019-07-05 16:07:55 +0200

    fix widget landing page excerpts

--- apps/widget/modules/widget/templates/_excerpt.php
+++ apps/widget/modules/widget/templates/_excerpt.php
@@ -1,18 +1,10 @@
-<div id="widget-action-id-<?php echo $excerpt['petition_id'] ?>" class="excerpt span6 top_line" onclick="<?php echo UtilWidget::getWidgetHereJs($excerpt['widget_id'], true) ?>" style="cursor: pointer">
+<div id="widget-action-id-<?php echo $excerpt['petition_id'] ?>" class="excerpt col-md-6 top_line" onclick="<?php echo UtilWidget::getWidgetHereJs($excerpt['widget_id'], true) ?>" style="cursor: pointer">
     <div class="row">
-        <div class="span1"><?php if ($excerpt['key_visual']): ?><img class="home_teaser" src="<?php echo image_path('keyvisual/' . $excerpt['key_visual']) ?>" alt="" /><?php else: ?><div class="visible-desktop" style="background: #f5f5f5; height: 70px">&nbsp;</div><?php endif ?></div>
-        <div class="span5">
+        <div class="col-md-3"><?php if ($excerpt['key_visual']): ?><img class="home_teaser" src="<?php echo image_path('keyvisual/' . $excerpt['key_visual']) ?>" alt="" style="max-width:100%" /><?php endif ?></div>
+        <div class="col-md-7">
             <div class="petition_stats_container">
                 <p><b><?php echo  Util::enc($excerpt['title']) ?></b> <?php echo Util::enc($excerpt['text']) ?></p>
-                <div class="petition_stats">
-                    <dl class="well well-small">
-                        <dt>Total</dt>
-                        <dd title="<?php echo number_format($excerpt['signings'], 0, '.', ',') ?>"><?php echo Util::readable_number($excerpt['signings']) ?></dd>
-                        <dt>Today</dt>
-                        <dd title="<?php echo number_format($excerpt['signings24'], 0, '.', ',') ?>"><?php echo Util::readable_number($excerpt['signings24']) ?></dd>
-                    </dl>
-                    <a class="btn btn-sm show">sign!</a>
-                </div>
+                <a class="btn btn-sm btn-secondary d-block show">sign!</a>
             </div>
         </div>
     </div>
