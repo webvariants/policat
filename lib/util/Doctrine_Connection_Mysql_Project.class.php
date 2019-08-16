73437bf91f896f2b110c9fdbb58345bdbd428651
Martin Schnabel <martin@webvariants.de>
2019-08-16 10:54:32 +0200

    fix mysql identifier quoting

--- lib/util/Doctrine_Connection_Mysql_Project.class.php
+++ lib/util/Doctrine_Connection_Mysql_Project.class.php
@@ -5,6 +5,13 @@ class Doctrine_Connection_Mysql_Project extends Doctrine_Connection_Mysql {
 
     public function quoteIdentifier($str, $checkOption = true)
     {
+        switch ($str) {
+            case 'member':
+            case 'groups':
+            case 'function':
+                return '`' . $str . '`';
+        }
+
         return $str;
     }
 }
