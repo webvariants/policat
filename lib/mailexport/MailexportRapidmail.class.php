0c00c98f9387b75c60ca70e4b0d673b497d4fb51
Martin Schnabel <martin@webvariants.de>
2019-10-09 16:08:57 +0200

    mailexport enable crontab, helptext #101

--- lib/mailexport/MailexportRapidmail.class.php
+++ lib/mailexport/MailexportRapidmail.class.php
@@ -6,9 +6,10 @@ class MailExportRapidmail extends MailExport {
   public function formSetup(MailExportSettingForm $form) {
     $form->setWidget('rapidmail_enabled', new WidgetBoolean(['label' => 'Enable Rapidmail']));
     $form->setValidator('rapidmail_enabled', new sfValidatorBoolean());
+    $form->getWidgetSchema()->setHelp('rapidmail_enabled', 'Once activated, this API submits subscriber data to RapidMail once validated by opt-in email. Note that you have to manually import subscriber data that you collected before you activated this API. Add your RapidMail API account credentials and the ID of the list PoliCAT is supposed to add subscribers to (find the ID in the URL of your RapidMail list). As RapidMail\'s individual API restricts the use of the API to max 10 updates per second, PoliCAT uses the bulk/list option: once every hour, PoliCAT sends all subscriber data validated within that hour. To stop RapidMail sending you an email every time PoliCAT sends an update, you might want to uncheck the "Notification" option in RapidMail. Data includes email, first and last name (or full name), and if applicable, gender and post code. You may define additional fields in your Rapidmail list for signing date, country, widget-ID and URL-referer (the link to the page where the subscriber took part in your action). For OpenECI actions, you might want to track "thank-you-page-shown" as an approximate indicator if a subscriber submitted a statement of support via OpenECI.');
 
     $form->setWidget('rapidmail_username', new sfWidgetFormInputText(array('label' => 'Username')));
-    $form->setValidator('rapidmail_username', new sfValidatorString(array('max_length' => 100)));
+    $form->setValidator('rapidmail_username', new sfValidatorString(array('max_length' => 100, 'required' => false)));
 
     $form->setWidget('rapidmail_password', new sfWidgetFormInputPassword(array('label' => 'Password', 'always_render_empty' => true), array('autocomplete' => 'off', 'placeholder' => $form->getDefault('rapidmail_password') ? 'keep old password' : '')));
     $form->setValidator('rapidmail_password', new sfValidatorString(array('max_length' => 100, 'required' => false, 'empty_value' => $form->getDefault('rapidmail_password'))));
