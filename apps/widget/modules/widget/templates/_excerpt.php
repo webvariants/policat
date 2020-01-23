66f260fff1e22572ce002d5c4cf7362bffff114d
Martin Schnabel <martin@webvariants.de>
2020-01-23 12:02:21 +0100

    landing page teaser style #169

--- apps/widget/modules/widget/templates/_excerpt.php
+++ apps/widget/modules/widget/templates/_excerpt.php
@@ -1,4 +1,4 @@
-<div id="widget-action-id-<?php echo $excerpt['petition_id'] ?>" class="excerpt col-md-6 top_line" onclick="<?php echo UtilWidget::getWidgetHereJs($excerpt['widget_id'], true) ?>" style="cursor: pointer">
+<div id="widget-action-id-<?php echo $excerpt['petition_id'] ?>" class="excerpt col-md-6 top_line mb-4" onclick="<?php echo UtilWidget::getWidgetHereJs($excerpt['widget_id'], true) ?>" style="cursor: pointer">
     <div class="row">
         <div class="col-md-3"><?php if ($excerpt['key_visual']): ?><img class="home_teaser" src="<?php echo image_path('keyvisual/' . $excerpt['key_visual']) ?>" alt="" style="max-width:100%" /><?php endif ?></div>
         <div class="col-md-7">
